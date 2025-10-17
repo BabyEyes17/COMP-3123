2dfca52ad7abfaad3caf89cb4abc29856242b4b8
Jayden Lewis <jaydent.lewis@gmail.com>
2025-10-17 18:36:20 -0400

    Update

--- lab_exercises/lab_07/labweek09/src/App.js
+++ lab_exercises/lab_07/labweek09/src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Hello from './components/Hello';
 import Student from './components/Student';
 import Employee from './components/Employee';
 
 function App() {
-  let name = "Professor Jayden Lewis";
+  
+  const [info] = useState({
+    course: 'Fullstack Development - I',
+    lab: 'React JS Programming Week09 Lab Exercise',
+    id: '101484621',
+    name: 'Jayden Lewis',
+    college: 'George Brown College, Toronto'
+  });
+
   let myStyle = { color: 'blue', backgroundColor: 'yellow' };
 
   return (
     <Fragment>
+      
       {/* 
       <h1 style={{ color: 'red', backgroundColor: 'yellow' }}>Hello ReactJS</h1>
       <h2 style={myStyle}>{name}</h2>
@@ -21,16 +30,18 @@ function App() {
 
       <div className="App">
         <header className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
           
-          <h1>Welcome To Fullstack Development - I </h1>
-          <h2>React JS Programming Week09 Lab Exercise</h2>
-          <h3>101484621</h3>
-          <h5>Jayden Lewis</h5>
-          <h6>George Brown College, Toronto</h6>
+          <h1>Welcome To {info.course}</h1>
+          <h2>{info.lab}</h2>
+          <h3>{info.id}</h3>
+          <h5>{info.name}</h5>
+          <h6>{info.college}</h6>
 
         </header>
       </div>
+
     </Fragment>
   );
 }
